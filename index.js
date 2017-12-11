2bcf6cbe220d311b619badc1683186384bf94011
Silvia Chung <silviachung@Silvias-MacBook.local>
2017-12-11 18:19:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  const result = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  return result;
+}
+
+function fuzzyMatch(drivers, letter) {
+  const result = drivers.filter(driver => driver.slice(0, 2) === letter);
+
+  return result;
+}
+
+function matchName(drivers, string) {
+  const result = drivers.filter(driver => driver.name === string);
+  return result;
+}
