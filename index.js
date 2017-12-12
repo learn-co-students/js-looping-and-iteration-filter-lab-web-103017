a799a28c08fb8c8277e1e1a411a511f321365126
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-12-11 19:11:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function(dName){
+    return name.toLowerCase()=== dName.toLowerCase()
+  });
+}
+
+function fuzzyMatch (drivers, letters) {
+  let lettersLength = letters.length;
+  return drivers.filter(function (driverName) {
+    return driverName.slice(0, lettersLength) === letters;
+  });
+}
+
+function matchName(data, string) {
+  return data.filter(function(obj) {return obj.name === string;
+});
+}
