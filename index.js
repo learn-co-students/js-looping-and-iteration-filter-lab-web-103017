01793bfbe7110ea9b15a767a4928df594ff4ff64
lierica <ericaaa.li@gmail.com>
2017-12-11 21:51:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(array, string){
+	return array.filter(function(name) { return name.toLowerCase() === string.toLowerCase()})
+}
+
+function fuzzyMatch(array, string){
+	return array.filter(function(name) { return name.startsWith(string)})
+}
+
+function matchName(array, string){
+	return array.filter(function(person) {return person.name === string})
+}
