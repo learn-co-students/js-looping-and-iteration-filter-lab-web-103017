b180d18c90e9801a7dedc957f7cf040abb7fe3b4
psarma89 <priyam.sarma@gmail.com>
2017-12-11 19:28:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  let output = [];
+  for (const driver of drivers){
+    if (name.toLowerCase() === driver.toLowerCase()) {
+      output.push(driver)
+    }
+  }
+  return output
+}
+
+function fuzzyMatch(drivers, name){
+    let output = [];
+    for (const driver of drivers){
+      if (name === driver.slice(0,name.length)) {
+        output.push(driver)
+      }
+    }
+    return output
+}
+
+function matchName(drivers, name){
+    let output = [];
+    for (const driver of drivers){
+      if (name === driver.name) {
+        output.push(driver)
+      }
+    }
+    return output
+}
