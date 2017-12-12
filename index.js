31af17f10db67cb618d05342778cf44a19401b18
psarma89 <priyam.sarma@gmail.com>
2017-12-11 19:44:45 -0500

    Adds solution

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-  let output = [];
-  for (const driver of drivers){
-    if (name.toLowerCase() === driver.toLowerCase()) {
-      output.push(driver)
-    }
-  }
-  return output
+  return drivers.filter(function(driverName){
+    return name.toLowerCase() === driverName.toLowerCase();
+  });
 }
 
 function fuzzyMatch(drivers, name){
-    let output = [];
-    for (const driver of drivers){
-      if (name === driver.slice(0,name.length)) {
-        output.push(driver)
-      }
-    }
-    return output
+  return drivers.filter(function(driverName){
+    return name === driverName.slice(0,name.length)
+  });
 }
 
 function matchName(drivers, name){
-    let output = [];
-    for (const driver of drivers){
-      if (name === driver.name) {
-        output.push(driver)
-      }
-    }
-    return output
+  return drivers.filter(function(driver){
+    return driver.name === name
+  });
 }
