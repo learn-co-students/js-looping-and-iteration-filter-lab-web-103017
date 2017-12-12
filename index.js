b3fd886c37ebbfac8d06c4f56a54eaad340170a0
github username <github email address>
2017-12-12 11:35:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file
+function findMatching(drivers, name)
+{
+  const newArray = [];
+  for(let i = 0; i < drivers.length; i++)
+  {
+    if(drivers[i].toLowerCase() === name.toLowerCase())
+    {
+      newArray.push(drivers[i]);
+    }
+  }
+  return newArray;
+}
+
+function fuzzyMatch(drivers, letters)
+{
+  const newArray = [];
+  for(let i = 0; i < drivers.length; i++)
+  {
+    if(sliceItem(drivers[i]) === letters.toLowerCase())
+    {
+      newArray.push(drivers[i]);
+    }
+  }
+  return newArray;
+}
+function sliceItem(item)
+{
+ return item.slice(0, 2).toLowerCase();
+}
+
+function matchName(drivers, name)  
+{
+  newObject = []
+  for(let i = 0; i < drivers.length; i++)
+  {
+    if (drivers[i]["name"] === name)
+    {
+      newObject.push(drivers[i])
+
+    }
+  }
+  return newObject
+
+}
